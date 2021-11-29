00a62124e66b228156ecc919d5f2978ea3204377
izaelgs <izael-123@outlook.com>
2021-11-28 22:23:38 -0300

    inserção de dados no banc ode forma segura

--- src/database/Conexao.java
+++ src/database/Conexao.java
@@ -1,20 +1,41 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Conexao {
-	public static void teste() throws SQLException {
+	public static void insertUser(Integer status, String nome, String login, String senha,String email, String telefone, String cpf, String url) throws SQLException {
+		Connection conexao = Fabrica.getConexao();		
+		
+		String sql = "INSERT INTO user(status, nome, login, senha, email, telefone, cpf, img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setInt(1, status);
+		stmt.setString(2, nome);
+		stmt.setString(3, login);
+		stmt.setString(4, senha);
+		stmt.setString(5, email);
+		stmt.setString(6, telefone);
+		stmt.setString(7, cpf);
+		stmt.setString(8, url);
+		
+		stmt.execute();
+		
+		System.out.println("dado inserido com sucesso");
+		conexao.close();
+	}
+	
+	public static void deletetUser(Integer id) throws SQLException {
 		Connection conexao = Fabrica.getConexao();
 		
-		String sql = "INSERT INTO user(status, nome, login, senha, email, telefone, cpf, img)\r\n"
-				+ "VALUES(1, \"fulano 1\", \"fulano.teste\", \"senha\", \"email.com\", \"2798876654\", \"16654326785\", \"https:://www.google.com\")";
+		String sql = "DELETE FROM user WHERE id = "+id+"";
 		
 		Statement stmt = conexao.createStatement();
 		stmt.execute(sql);
 		
-		System.out.println("dado inserido com sucesso");
+		System.out.println("dado deletado com sucesso");
 		conexao.close();
-	}	
+	}
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -6,6 +6,8 @@ import database.Conexao;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
-		Conexao.teste();
+		Conexao.insertUser(1, "inserção segura", "insert", "pinto", "disgramera.sexo", "2797783164", "15564576915", "https://xvideos.com/imagem.png");
+		
+		Conexao.deletetUser(3);
 	}
 }
