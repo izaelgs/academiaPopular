d1e0aa06bb87f96a72d4b9fa968cf08879d7804d
izaelgs <izael-123@outlook.com>
2021-11-28 21:00:38 -0300

    import inicial

---
+++ src/database/Fabrica.java
@@ -0,0 +1,18 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Fabrica {
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+			final String usuario = "root";
+			final String senha = "senha";
+			return DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
